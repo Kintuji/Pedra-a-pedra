d131a736dd7cbb62f3f23280ec89db54c02faeed
Tiago Martins <tiago.rvm06@gmail.com>
2026-03-25 00:57:31 +0000

    tv doing

---
+++ Assets/Scripts/TVController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TVController : MonoBehaviour
+{
+    [SerializeField] VideoPlayer _videoPlayer;
+    [SerializeField] Material _onMaterial;
+    [SerializeField] Material _offMaterial;
+    [SerializeField] Renderer _render;
+
+    private bool _isOn = true;
+
+    private void Awake()
+    {
+     //   _render = GetComponent<Renderer>();
+    }
+
+    public void ToggleTV()
+    {
+        Debug.Log("Turning TV Off");
+        if (_isOn)
+        {
+            Debug.Log("Turning TV Off");
+            _render.material = _offMaterial;
+            _videoPlayer.Stop();
+            _isOn = false;
+
+        }
+        else
+        {
+            _render.material = _onMaterial;
+            _videoPlayer.Prepare();
+            _isOn = true;
+        }
+    }
+
+    public void TogglePlayPause()
+    {
+        if (!_isOn) return;
+
+        if (_videoPlayer.isPlaying)
+        {
+            _videoPlayer.Pause();
+        }
+        else
+        {
+            if (!_videoPlayer.isPrepared)
+            {
+                _videoPlayer.Prepare();
+                return;
+            }
+
+            _videoPlayer.Play();
+        }
+    }
+    public void RestartVideo() 
+    { 
+        if (!_isOn) return;
+        _videoPlayer.Stop();
+        _videoPlayer.Prepare();
+        _videoPlayer.Play();
+    }
+}
