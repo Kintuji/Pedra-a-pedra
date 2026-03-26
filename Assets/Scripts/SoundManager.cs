929598e2cd8c0f83a6f1c31b3560575d05977790
WazZzabiii <miggas.rebelo@gmail.com>
2026-03-26 17:28:19 +0000

    FInalizado Sound Manager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+
+public enum SoundType
+{
+    MAINMENU,
+    AMBIENCE
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] soundList;
+    private static SoundManager instance;
+    public AudioSource audioSource;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public static void PlaySound(SoundType sound, float volume = 1)
+    {
+        instance.audioSource.PlayOneShot(instance.soundList[(int)sound], volume);
+     }
+}
