330e02c8e0abd718a5eac173016fe6b8ff72f6fa
WazZzabiii <miggas.rebelo@gmail.com>
2026-03-26 16:21:33 +0000

    Sound Manager Script

---
+++ Assets/Scripts/Sound_Manager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Sound_Manager : MonoBehaviour
+{
+
+    public static Sound_Manager instance;
+    public AudioSource AudioSource;
+    public AudioClip AudioClip;
+    //private AudioSource AudioSource = new AudioSource[32];    // o gajo tinha isto mas tava a dar erro então nao sei logo ves se é preciso
+    public void PlayMainMenuMusic()
+    {
+
+    }
+
+    public void PlayAmbienceMusic()
+    {
+
+    }
+}
